6f7d465cc8be9236f86b739fffecf0d7794965cc
Adriaan Cody Schuffelen <astronomix@gmail.com>
2014-02-21 16:15:31 -0600

    Added some dummy resource hexes to have something to look at

--- src/GameBoard.cpp
+++ src/GameBoard.cpp
@@ -7,7 +7,13 @@ using std::map;
 using std::unique_ptr;
 
 GameBoard::GameBoard() {
-	
+	pieces.emplace(Coordinate(0, 0), unique_ptr<GamePiece>(new ResourceTile(*this, ResourceTile::WOOD, 2)));
+	pieces.emplace(Coordinate(1, 1), unique_ptr<GamePiece>(new ResourceTile(*this, ResourceTile::SHEEP, 2)));
+	pieces.emplace(Coordinate(-1, -1), unique_ptr<GamePiece>(new ResourceTile(*this, ResourceTile::BRICK, 2)));
+	pieces.emplace(Coordinate(2, -1), unique_ptr<GamePiece>(new ResourceTile(*this, ResourceTile::ORE, 2)));
+	pieces.emplace(Coordinate(-2, 1), unique_ptr<GamePiece>(new ResourceTile(*this, ResourceTile::DESERT, 2)));
+	pieces.emplace(Coordinate(1, -2), unique_ptr<GamePiece>(new ResourceTile(*this, ResourceTile::GRAIN, 2)));
+	pieces.emplace(Coordinate(-1, 2), unique_ptr<GamePiece>(new ResourceTile(*this, ResourceTile::BRICK, 2)));
 }
 
 GameBoard::~GameBoard() {
