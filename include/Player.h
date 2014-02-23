390ca20152508e94bec566477d0e9bef582e71b9
ankitbhutani <ankitbhutani@wirelessprvnat-172-16-196-122.near.illinois.edu>
2014-02-23 14:25:18 -0600

    //Player(with resources and victory points), DevCards and Deck

--- include/Player.h
+++ include/Player.h
@@ -1,25 +1,70 @@
+//
+//  Player.h
+//  Catan parts
+//
+//  Created by Ankit Bhutani on 2/22/14.
+//  Copyright (c) 2014 Ankit Bhutani. All rights reserved.
+//
+
 #ifndef PLAYER_H
 #define PLAYER_H
 
+
+#include <vector>
 #include <string>
 
-#include "Resources.h"
+//#include "DevelopmentCard.h"
+
+
+class DevelopmentCard;
+class Deck;
+
+enum ResourceType {WOOD, BRICK, ORE, WHEAT, WOOL};
+
+
+
+//#include <ext/hash_map>
+//#include <unordered_map>
 
 class Player {
 private:
-	std::string name;
-	Resources resources;
+    std::string name;
+
+    std::vector<DevelopmentCard*> developmentCards;
+
+    int armySize;
+    int longestRoad;
+    int victoryPoints;
+
+    int resources[5];
+
+
 public:
-	Player(const std::string& name);
-	Player(Player&) = delete;
+
+	Player(std::string playerName);
 	~Player();
-	Player& operator=(Player&) = delete;
-	
-	const std::string& getName() const;
-	void setName(const std::string& newName);
-	
-	const Resources& getResources() const;
-	void setResources(const Resources& newResources);
+
+    int getVictoryPoints();
+    void updateVictoryPoints();
+
+    int getDevCardsInHand();
+
+    void buyCard(DevelopmentCard* card);
+    void playCard(DevelopmentCard* card);
+
+    int getWood();
+    int getBrick();
+    int getOre();
+    int getWheat();
+    int getWool();
+
+    void setWood(int resource);
+    void setBrick(int resource);
+    void setOre(int resource);
+    void setWheat(int resource);
+    void setWool(int resource);
+
+
 };
 
 #endif
--- src/Player.cpp
+++ src/Player.cpp
@@ -1,27 +1,110 @@
+//
+//  Player.cpp
+//  Catan parts
+//
+//  Created by Ankit Bhutani on 2/22/14.
+//  Copyright (c) 2014 Ankit Bhutani. All rights reserved.
+//
+
+
 #include "Player.h"
+#include "DevelopmentCard.h"
 
-using std::string;
 
-Player::Player(const string& name) : name(name), resources() {
-	
+Player::Player(std::string playerName)
+{
+	armySize = 0;
+	longestRoad = 0;
+	victoryPoints = 0;
 }
 
-Player::~Player() {
-	
+int Player::getDevCardsInHand()
+{
+	return developmentCards.size();
 }
 
-const string& Player::getName() const {
-	return name;
+
+void Player::updateVictoryPoints()
+{
+    //TODO: Calculate and Update victory points
 }
 
-void Player::setName(const string& newName) {
-	name = newName;
+int Player::getVictoryPoints()
+{
+    updateVictoryPoints();
+    return victoryPoints;
 }
 
-const Resources& Player::getResources() const {
-	return resources;
+
+void Player::buyCard(DevelopmentCard *card)
+{
+    developmentCards.push_back(card);
 }
 
-void Player::setResources(const Resources& newResources) {
-	resources = newResources;
+void Player::playCard(DevelopmentCard *card)
+{
+    if(std::find(developmentCards.begin(), developmentCards.end(), card) == developmentCards.end()) {
+        return;
+    }
+    card->playCard();
+    if (card->getType() == KNIGHT)
+        armySize++;
+
+    developmentCards.erase(std::remove(developmentCards.begin(), developmentCards.end(), card), developmentCards.end());
+
+}
+
+
+
+
+int Player::getWood()
+{
+    return resources[0];
+}
+
+int Player::getBrick()
+{
+    return resources[1];
+}
+
+int Player::getOre()
+{
+    return resources[2];
+}
+
+int Player::getWheat()
+{
+    return resources[3];
+}
+
+int Player::getWool()
+{
+    return resources[4];
+}
+
+
+
+void Player::setWood(int resource)
+{
+    resources[0] = resource;
+}
+
+void Player::setBrick(int resource)
+{
+    resources[1] = resource;
+}
+
+void Player::setOre(int resource)
+{
+    resources[2] = resource;
+}
+
+void Player::setWheat(int resource)
+{
+    resources[3] = resource;
+}
+
+void Player::setWool(int resource)
+{
+    resources[4] = resource;
 }
