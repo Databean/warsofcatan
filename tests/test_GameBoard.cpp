4fd81e505a028e314a8e44a07878e617ae1a0f33
Marty Malecki <malecki1@illinois.edu>
2014-03-05 17:32:00 -0600

    Finished polishing roll checking tests

--- tests/test_GameBoard.cpp
+++ tests/test_GameBoard.cpp
@@ -24,13 +24,15 @@ TEST(loadFile_simple){
 TEST(randomize_rolls_fail)
 {
     GameBoard * test_board = new GameBoard;
-    CHECK(test_board->testRollChecking({0, 2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12}) == false);
+    int badRolls[] = {0, 2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12};
+    CHECK(test_board->testRollChecking(badRolls) == false);
     delete(test_board);
 }
 
 TEST(randomize_rolls_pass)
 {
     GameBoard * test_board = new GameBoard;
-    CHECK(test_board->testRollChecking({9, 11, 5, 4, 0, 3, 4, 2, 10, 8, 3, 6, 9, 11, 5, 10, 6, 12, 8}) == true);
+    int goodRolls[] = {9, 11, 5, 4, 0, 3, 4, 2, 10, 8, 3, 6, 9, 11, 5, 10, 6, 12, 8};
+    CHECK(test_board->testRollChecking(goodRolls) == true);
     delete(test_board);
 }
