0aee6e069c08971de9532c65586d27b23baa841e
A. Cody Schuffelen <astronomix@gmail.com>
2014-03-06 10:03:10 -0600

    Replaced dynamic allocation in tests with static allocation

--- tests/test_GameBoard.cpp
+++ tests/test_GameBoard.cpp
@@ -10,29 +10,25 @@
 
 
 TEST(saveFile_simple){
-	GameBoard * test_board = new GameBoard;
-	CHECK(test_board->save_Board("test_1") == 0);
-	delete(test_board);
+	GameBoard test_board;
+	CHECK(test_board.save_Board("test_1") == 0);
 }
 
 TEST(loadFile_simple){
-	GameBoard * test_board = new GameBoard;
-	CHECK(test_board->load_Board("test_1") == 0);
-	delete(test_board);
+	GameBoard test_board;
+	CHECK(test_board.load_Board("test_1") == 0);
 }
 
 TEST(randomize_rolls_fail)
 {
-    GameBoard * test_board = new GameBoard;
+    GameBoard test_board;
     int badRolls[] = {0, 2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12};
-    CHECK(test_board->testRollChecking(badRolls) == false);
-    delete(test_board);
+    CHECK(test_board.testRollChecking(badRolls) == false);
 }
 
 TEST(randomize_rolls_pass)
 {
-    GameBoard * test_board = new GameBoard;
+    GameBoard test_board;
     int goodRolls[] = {9, 11, 5, 4, 0, 3, 4, 2, 10, 8, 3, 6, 9, 11, 5, 10, 6, 12, 8};
-    CHECK(test_board->testRollChecking(goodRolls) == true);
-    delete(test_board);
+    CHECK(test_board.testRollChecking(goodRolls) == true);
 }
