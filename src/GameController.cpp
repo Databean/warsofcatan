3178108bcd7d1bf749795e5247f56e76875e8e17
Paul Graff <banana.manwich@gmail.com>
2014-04-20 14:08:39 -0500

    Modified placement order of buttons so that the board is added first, and thus all board clicks are handled first. Still need to do something more permanent to fix the issue with button overlap.

--- src/GameController.cpp
+++ src/GameController.cpp
@@ -17,12 +17,11 @@
 GameController::GameController(GameBoard& model, GameView& view) : model(model), view(view) {
 	using namespace std::placeholders;
 
+	view.addElement(makeViewButton(std::bind(&GameController::handleBoardEvent, this, _1), {{0, 0}, {1, 1}}));
 
 	view.addElement(makeViewButtonColor(std::bind(&GameController::nextTurn, this, _1), {{0, 0.2}, {0.1, 0.3}}, std::make_tuple(0.f, 0.f, 1.f)));
 	view.addElement(makeViewButtonColor(std::bind(&GameController::handleRoadButtonEvent, this, _1), {{0, 0}, {0.1, 0.1}}, std::make_tuple(1.f, 0.f, 0.f)));
 	view.addElement(makeViewButtonColor(std::bind(&GameController::handleSettlementButtonEvent, this, _1), {{0, 0.1}, {0.1, 0.2}}, std::make_tuple(0.f, 1.0f, 0.f)));
-
-	view.addElement(makeViewButton(std::bind(&GameController::handleBoardEvent, this, _1), {{0, 0}, {1, 1}}));
 
 	auto font = "resources/TypeWritersSubstitute-Black.ttf";
 	auto fontSize = 50;
@@ -158,7 +157,6 @@ bool GameController::handleBoardEvent(ScreenCoordinate screenCoord) {
 
 	switch (getState()){
 	case BUILDROAD:
-		std::cout << "BUILDROAD\n";
 		if(!hasClickHistory()) {
 			storeClick(coord);
 		} else {
