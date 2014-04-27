fa2f10cfc0f4453e10fe9c9ce929ab9432d57d3b
Alex <a.m.politakis@gmail.com>
2014-04-26 20:48:01 -0500

    rolling a 7 now robs half of all player's resources iff they have more than 7 resource cards

--- src/GameController.cpp
+++ src/GameController.cpp
@@ -177,6 +177,8 @@ bool GameController::handleBoardEvent(ScreenCoordinate screenCoord) {
 		break;
 	case ROBBER:
 		//model.moveRobber(coord);
+		robPlayers();
+
 		if(!hasClickHistory())
 			storeClick(coord);
 		
@@ -215,7 +217,7 @@ bool GameController::handleBoardEvent(ScreenCoordinate screenCoord) {
 			} 
 
 		//TODO Decrement knight count
-			
+
 
 		
 		popState();
@@ -246,6 +248,26 @@ bool GameController::handleBoardEvent(ScreenCoordinate screenCoord) {
 		break;
 	}
 	return true;
+}
+
+void GameController :: robPlayers() {
+	for (int i = 0; i < model.getNoOfPlayers(); i++) {
+		int resources[5];
+		model.getPlayer(i).checkResources(resources);
+		//int rSum = resources[0] + resources[1] + resources[2] + resources[3] + resources[4];
+		int rSum = model.getPlayer(i).getWood() + 
+		model.getPlayer(i).getOre() + 
+		model.getPlayer(i).getBrick() + 
+		model.getPlayer(i).getWheat() + 
+		model.getPlayer(i).getWool(); 
+
+		if (rSum > 7) {
+			for (int j = 0 ; j < rSum/2; j++) {
+				model.getPlayer(i).addResource(model.getPlayer(i).getRandomResource(), -1);
+
+			}
+		}
+	}
 }
 
 /**
