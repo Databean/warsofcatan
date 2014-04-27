7640fedb9e7bf61d398e9a462d0e388777668dfa
A. Cody Schuffelen <astronomix@gmail.com>
2014-04-26 20:54:36 -0500

    Made parameter generation not use testing::Combine

--- tests/testRenderer.cpp
+++ tests/testRenderer.cpp
@@ -1,25 +1,29 @@
 #include <iostream>
-#include <tuple>
-
-#define GTEST_USE_OWN_TR1_TUPLE 0
-#define GTEST_ENV_HAS_STD_TUPLE_ 1
+#include <vector>
 
 #include "gtest/gtest.h"
 
 #include "Renderer.h"
 
+using std::vector;
 using std::make_pair;
 
-using std::tuple;
-using std::get;
-
-class CoordinateConversionTest : public ::testing::TestWithParam<tuple<int, int>> {
+class CoordinateConversionTest : public ::testing::TestWithParam<Coordinate> {
 	
 };
 
 TEST_P(CoordinateConversionTest, IsReversable) {
-	auto pair = make_pair(get<0>(GetParam()), get<1>(GetParam()));
-	ASSERT_EQ(pair, screenToCoord(coordToScreen(pair)));
+	ASSERT_EQ(GetParam(), screenToCoord(coordToScreen(GetParam())));
 }
 
-INSTANTIATE_TEST_CASE_P(LargeSquare, CoordinateConversionTest, ::testing::Combine(::testing::Range(-10, 10), ::testing::Range(-10, 10)));
+vector<Coordinate> makeInputs() {
+	vector<Coordinate> ret;
+	for(int x = -5; x < 5; x++) {
+		for(int y = -5; y < 5; y++) {
+			ret.push_back(Coordinate{x, y});
+		}
+	}
+	return std::move(ret);
+}
+
+INSTANTIATE_TEST_CASE_P(LargeSquare, CoordinateConversionTest, ::testing::ValuesIn(makeInputs()));
